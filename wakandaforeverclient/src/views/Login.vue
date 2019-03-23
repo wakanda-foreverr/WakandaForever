781c301a4998817161a53dcdb48eda2542b40046
Katerina Simeonova <katherine.simeonova@gmail.com>
2019-03-23 18:42:09 +0200

    Add on submit event on the login

--- wakandaforeverclient/src/views/Login.vue
+++ wakandaforeverclient/src/views/Login.vue
@@ -3,7 +3,7 @@
         <div class="limiter">
             <div class="container-login100">
                 <div class="wrap-login100">
-                    <form class="login100-form validate-form p-l-55 p-r-55 p-t-178">
+                    <form v-on:submit.prevent="loginUser" class="login100-form validate-form p-l-55 p-r-55 p-t-178">
 					<span class="login100-form-title">
 						Sign in
 					</span>
