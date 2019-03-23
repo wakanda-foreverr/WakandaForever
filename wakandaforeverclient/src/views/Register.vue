6ba88c86e9546f5ad5d4353c7c5ec14f66672c39
Katerina Simeonova <katherine.simeonova@gmail.com>
2019-03-23 14:57:45 +0200

    Add Register user logic

--- wakandaforeverclient/src/views/Register.vue
+++ wakandaforeverclient/src/views/Register.vue
@@ -3,28 +3,28 @@
         <div class="limiter">
             <div class="container-login100">
                 <div class="wrap-login100">
-                    <form class="login100-form validate-form p-l-55 p-r-55 p-t-178">
+                    <form v-on:submit.prevent="registerUser" class="login100-form validate-form p-l-55 p-r-55 p-t-178">
 					<span class="login100-form-title">
 						Register
 					</span>
 
                         <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
-                            <input class="input100" type="text" name="username" placeholder="Username">
+                            <input v-model="username" class="input100" type="text" name="username" placeholder="Username">
                             <span class="focus-input100"></span>
                         </div>
 
                         <div class="wrap-input100 validate-input" data-validate = "Please enter password">
-                            <input class="input100" type="password" name="pass" placeholder="Password">
+                            <input v-model="password" class="input100" type="password" name="pass" placeholder="Password">
                             <span class="focus-input100"></span>
                         </div>
 
                         <div class="wrap-input100 validate-input" data-validate = "Please confirm password">
-                            <input class="input100" type="password" name="passconfirm" placeholder="Confirm-Password">
+                            <input class="input100" type="password" name="passconfirm" placeholder="Confirm Password">
                             <span class="focus-input100"></span>
                         </div>
 
                         <div class="container-login100-form-btn">
-                            <button class="login100-form-btn">
+                            <button type="submit" class="login100-form-btn">
                                 Register
                             </button>
                         </div>
@@ -38,7 +38,22 @@
 
 <script>
     export default {
-        name: "Register.vue"
+        name: "Register.vue",
+        data() {
+            return {
+                username: '',
+                password: ''
+            }
+        },
+        methods: {
+            registerUser() {
+                let newUser = {
+                    username: this.username,
+                    password: this.password
+                };
+                console.log(newUser);
+            }
+        }
     }
 </script>
 
