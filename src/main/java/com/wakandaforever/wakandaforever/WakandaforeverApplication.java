55b52c6fd5169ae76e1aef30eb99b1a75ecabd0a
siqqQ <b.bohosyan@gmail.com>
2019-03-23 12:29:26 +0200

    Initial commit

---
+++ src/main/java/com/wakandaforever/wakandaforever/WakandaforeverApplication.java
@@ -0,0 +1,13 @@
+package com.wakandaforever.wakandaforever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakandaforeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WakandaforeverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wakandaforever/wakandaforever/WakandaforeverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wakandaforever.wakandaforever;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WakandaforeverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
