482284eda5bf8b6a906fa1badf5e215a6bbc4373
Katerina Simeonova <katherine.simeonova@gmail.com>
2019-03-23 14:32:07 +0200

    Add register form html

--- wakandaforeverclient/src/views/Register.vue
+++ wakandaforeverclient/src/views/Register.vue
@@ -1,6 +1,38 @@
 <template>
     <div>
-        Register
+        <div class="limiter">
+            <div class="container-login100">
+                <div class="wrap-login100">
+                    <form class="login100-form validate-form p-l-55 p-r-55 p-t-178">
+					<span class="login100-form-title">
+						Register
+					</span>
+
+                        <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
+                            <input class="input100" type="text" name="username" placeholder="Username">
+                            <span class="focus-input100"></span>
+                        </div>
+
+                        <div class="wrap-input100 validate-input" data-validate = "Please enter password">
+                            <input class="input100" type="password" name="pass" placeholder="Password">
+                            <span class="focus-input100"></span>
+                        </div>
+
+                        <div class="wrap-input100 validate-input" data-validate = "Please confirm password">
+                            <input class="input100" type="password" name="passconfirm" placeholder="Confirm-Password">
+                            <span class="focus-input100"></span>
+                        </div>
+
+                        <div class="container-login100-form-btn">
+                            <button class="login100-form-btn">
+                                Register
+                            </button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
