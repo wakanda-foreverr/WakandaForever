f26ce2240e1ee6fa2e8078909c7d0df23f3d2618
Katerina Simeonova <katherine.simeonova@gmail.com>
2019-06-18 15:09:18 +0300

    Implement User Dashboard information list

--- wakandaforeverclient/src/serivces/DashboardService.js
+++ wakandaforeverclient/src/serivces/DashboardService.js
@@ -1,25 +1,43 @@
-import axios from 'axios'
-import cookie from 'vue-cookie'
-
-let apiClient = axios.create({
-    baseURL: `http://80.240.21.133:7777`,
-    withCredentials: false,
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + cookie.get("token"),
-        'Access-Control-Allow-Origin': '*'
-    },
-    timeout: 10000
-});
-
-export default {
-    getAllData: function () {
-        apiClient.get("/data/all/")
-            .then(
-            response => {
-                console.log(response.data)
-            }
-        );
-    }
-}
+// import axios from 'axios'
+// import cookie from 'vue-cookie'
+//
+// // let apiClient = axios.create({
+// //     baseURL: `http://80.240.21.133:7777`,
+// //     withCredentials: false,
+// //     headers: {
+// //         Accept: 'application/json',
+// //         'Content-Type': 'application/json',
+// //         'Authorization': 'Bearer ' + cookie.get("token"),
+// //         'Access-Control-Allow-Origin': '*'
+// //     },
+// //     timeout: 10000
+// // });
+//
+// // let sound = {
+// //     sound: ""
+// // }
+// //
+// // const getAllData = async function(){
+// //     apiClient.get("/data/all/")
+// //         .then(
+// //             response =>
+// //                 this.sound = response.data.sound
+// //                 // console.log(response.data)
+// //         )
+// // }
+// //
+// // module.exports = async function (){
+// //     await getAllData();
+// // }
+//
+//
+// // export default {
+// //     getAllData: function () {
+// //         apiClient.get("/data/all/")
+// //             .then(
+// //             response => {
+// //                 console.log(response.data)
+// //             }
+// //         );
+// //     }
+// // }
--- wakandaforeverclient/src/views/UserDashBoard.vue
+++ wakandaforeverclient/src/views/UserDashBoard.vue
@@ -22,11 +22,17 @@
                                     </tr>
                                     </thead>
                                     <tbody>
-                                        <td>Row</td>
-                                        <td>Row</td>
-                                        <td>Row</td>
-                                        <td>Row</td>
-                                        <td>Row</td>
+                                    <tr v-for="data in this.allData">
+                                        <td v-bind:key="sound">{{data.sound}}</td>
+                                        <td v-bind:key="temperature">{{data.temperature}}</td>
+                                        <td v-bind:key="humidity">{{data.humidity}}</td>
+                                        <td v-bind:key="airPressure">{{data.sound}}</td>
+                                        <td v-bind:key="fire">{{data.fire}}</td>
+                                        <td v-bind:key="light">{{data.light}}</td>
+                                        <td v-bind:key="soilMoisture">{{data.soilMoisture}}</td>
+                                        <td v-bind:key="waterLevel">{{data.waterLevel}}</td>
+                                        <td v-bind:key="airQuality">{{data.airQuality}}</td>
+                                    </tr>
                                     </tbody>
                                 </table>
                             </div>
@@ -37,30 +43,30 @@
         </div>
     </div>
 </template>
+
 <script>
-    import dashBoardService from "../serivces/DashboardService";
+    import axios from "axios";
+    import cookie from "vue-cookie";
+
+    axios.defaults.baseURL = 'http://80.240.21.133:7777';
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + cookie.get("token");
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
     export default {
-        name: "UserDashBoard",
-        data: function() {
+        data() {
             return {
-                allData: [{
-                    sound: "0"
-                }]
+                allData: null,
+                fields: ['Sound', 'Temperature','Humidity','Air Pressure','Fire','Light', 'Soil Moisture', 'Water Level', 'Air Quality']
             }
         },
-        methods: {
-            getAllData(){
-                this.allData = dashBoardService.getAllData();
-                console.log(this.allData);
-            }
-        },
-        mounted(){
-            this.getAllData();
+        mounted() {
+            axios.get('/data/all')
+                .then(response => (this.allData = response.data))
         }
     }
-</script>
 
+</script>
 
 <style scoped>
 
