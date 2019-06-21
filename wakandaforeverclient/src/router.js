cba23ec005acbef9983146e41d4538dd9828228d
Katerina Simeonova <katherine.simeonova@gmail.com>
2019-06-21 17:00:49 +0300

    Extract the data table into another vue component

--- wakandaforeverclient/src/router.js
+++ wakandaforeverclient/src/router.js
@@ -31,7 +31,7 @@ export default new Router({
     {
       path: "/dashboard",
       name: "dashboard",
-      component: () => import("./views/UserDashBoard.vue")
+      component: () => import("./views/UserDashboard.vue")
     },
     {
       path: "/docs",
---
+++ wakandaforeverclient/src/views/UserDashboard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="dashboard-main-wrapper">
+        <div class="ecommerce-widget">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <!--<ChartLine options="" series=""></ChartLine>-->
+                        <!--<chart-line></chart-line>-->
+                        <data-table></data-table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    // import ChartLine from "./ChartLine";
+    import DataTable from "./Datatable";
+
+    export default {
+        // components: {ChartLine, DataTable}
+        components: {DataTable}
+    }
+
+</script>
+
+<style scoped>
+
+</style>
