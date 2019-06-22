90d9dd34497a1630931b7042d06adb4e9dcebc7b
Katerina Simeonova <katherine.simeonova@gmail.com>
2019-06-22 18:26:02 +0300

    Update menu links

--- wakandaforeverclient/src/App.vue
+++ wakandaforeverclient/src/App.vue
@@ -1,36 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <a class="navbar-brand" href="index.html">Wakanda Forever API</a>
-      <router-link id="home" to="/">Home</router-link> |
-      <router-link id="docs" to="/docs">Documentaion</router-link> |
-      <router-link id="signin" to="/signin">Sign in</router-link> |
-      <router-link id="signup" to="/signup">Sign up</router-link> |
-      <router-link id="logout" to="/logout">Logout</router-link>
+    <div id="app">
+        <div id="nav">
+            <a class="navbar-brand" href="index.html">Demeter API</a>
+            <router-link v-if="!loggedIn" id="home" to="/">Home |</router-link>
+            <router-link v-if="loggedIn" id="dashboard" to="/dashboard">Dashboard |</router-link>
+            <router-link id="docs" to="/docs">Documentaion |</router-link>
+            <router-link v-if="loggedIn" id="logout" to="/logout">Logout</router-link>
+            <router-link v-if="!loggedIn" id="signin" to="/signin">Sign in |</router-link>
+            <router-link v-if="!loggedIn" id="signup" to="/signup">Sign up</router-link>
+        </div>
+        <router-view/>
     </div>
-    <router-view />
-  </div>
 </template>
 
 <script>
-  import cookie from "vue-cookie";
+    import cookie from "vue-cookie";
 
-  export default {
-    mounted() {
-      let signInLink = document.getElementById("signin");
-      let signUpLInk = document.getElementById("signup");
-      let logoutLink = document.getElementById("logout");
+    export default {
+        data() {
+            return {
+                loggedIn: false
+            }
+        },
+        mounted() {
+            if (cookie.get("token")) {
+                this.loggedIn = true;
 
-      if(cookie.get("token")){
-        signInLink.classList.add("hidden");
-        signUpLInk.classList.add("hidden");
-      }
-      else{
-        logoutLink.classList.add("hidden");
-      }
+            } else {
+                this.loggedIn = false;
+            }
 
+        }
     }
-  }
 </script>
 
 <!--<script src="./assets/vendor/jquery/jquery-3.3.1.min.js" />-->
@@ -57,40 +58,41 @@
 
 <style src="./assets/css/main.css"/>
 <style src="./assets/css/util.css"/>
-<style src="./assets/vendor/bootstrap/css/bootstrap.min.css" />
-<style src="./assets/vendor/fonts/circular-std/style.css" />
+<style src="./assets/vendor/bootstrap/css/bootstrap.min.css"/>
+<style src="./assets/vendor/fonts/circular-std/style.css"/>
 
 <style src="./assets/libs/css/style.css"/>
 <!--<style src="./assets/vendor/fonts/fontawesome/css/fontawesome-all.css" />-->
-<style src="./assets/vendor/charts/chartist-bundle/chartist.css" />
-<style src="./assets/vendor/charts/morris-bundle/morris.css" />
+<style src="./assets/vendor/charts/chartist-bundle/chartist.css"/>
+<style src="./assets/vendor/charts/morris-bundle/morris.css"/>
 <!--<style rel="stylesheet" href="/src/assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css">-->
-<style src="./assets/vendor/charts/c3charts/c3.css" />
+<style src="./assets/vendor/charts/c3charts/c3.css"/>
 <!--<style rel="stylesheet" href="/src/assets/vendor/fonts/flag-icon-css/flag-icon.min.css">-->
 
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #002b80;
-}
-#nav {
-  padding: 30px;
-}
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #002b80;
+    }
 
-a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    #nav {
+        padding: 30px;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
 
-.hidden {
-    visibility: hidden;
-}
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
+
+    .hidden {
+        visibility: hidden;
+    }
 </style>
